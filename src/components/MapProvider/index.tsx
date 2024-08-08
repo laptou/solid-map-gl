d205f206c985728e130e9bc9dfaf292e837505a1
Ibiyemi Abiodun <ibiyemi@intulon.com>
2024-08-08 01:31:10 -0400

    remove mapProvider global state

--- src/components/MapProvider/index.tsx
+++ src/components/MapProvider/index.tsx
@@ -2,18 +2,14 @@ import { createContext, useContext, ParentComponent } from 'solid-js'
 import { createStore } from 'solid-js/store'
 import type { Map } from '../MapGL'
 
-const [state, setState] = createStore({ map: null })
-
-export const MapContext = createContext([state])
+export const MapContext = createContext([{ map: null }])
 
 export const useMapContext = () => useContext(MapContext)
 
 export const MapProvider: ParentComponent<{
   map?: Map
 }> = (props) => {
-  props.map && setState('map', props.map)
-
   return (
-    <MapContext.Provider value={[state]}>{props.children}</MapContext.Provider>
+    <MapContext.Provider value={[{ map: props.map }]}>{props.children}</MapContext.Provider>
   )
 }
